a76c140c737c622c9839b0a94d3ce95c6a270bca
MariusGabriel73 <118460001+MariusGabriel73@users.noreply.github.com>
2023-05-29 23:46:45 +0300

    cerinta1.c

--- cerinta1.c
+++ cerinta1.c
@@ -1,32 +1,43 @@
 #include "cerinte.h"
-// scrie in fisierul r.out numele echipei
-void display(Node *head, FILE *r)
-{
-    while (head != NULL)
-    {
-        fprintf(r, "%s\n", head->team.name);
+
+void display(Node *head,FILE *g)
+{   
+    //fprintf(g,"Teams: \n");
+    while(head != NULL)
+    {   
+        fprintf(g ,"%s\n", head->team.name);
+        //printf("%s\n",head->team.name);
         head = head->next;
     }
-    fprintf(r, "\n");
+    fprintf(g,"\n");
 }
-// citeste din fisierul d.in datele si le introduce in lista ma folosesc de 2 vectori                                          pentru a introduce numele si prenumele jucatorilor si inca unul pentru numele echipei
-Team Read_The_Team(FILE *d)
+//functia citeste fiecare echipa din fisierul d.in si o adauga in lista 
+Team Read_The_Team(FILE *f)
 {
     Team team;
-    fscanf(d, "%d", &team.nr_players);
+    fscanf(f, "%d", &team.nr_players);
     team.players = (Player *)malloc(team.nr_players * sizeof(Player));
     team.teamPoints = 0;
+    // Folosim 2 vectori pentru a putea introduce în listă numele și prenumele jucătorilor
+    // și încă unul pentru numele echipei
     char *nume_echipa = calloc(100, sizeof(char));
     char nume[50];
     char prenume[50];
-    fgets(nume_echipa, 100, d);
+    fgets(nume_echipa, 100, f);
     team.name = (char *)calloc(strlen(nume_echipa) + 1, sizeof(char));
     strcpy(nume_echipa + strlen(nume_echipa) - 1, "\0");
     strcpy(team.name, nume_echipa + 1);
 
+    // Verificăm dacă există un spațiu la sfârșitul numelui echipei și îl eliminăm
+    int len = strlen(team.name);
+    if (len > 0 && team.name[len - 1] == ' ')
+    {
+        team.name[len - 1] = '\0';
+    }
+
     for (int i = 0; i < team.nr_players; i++)
     {
-        fscanf(d, "%s %s %d", nume, prenume, &team.players[i].points);
+        fscanf(f, "%s %s %d", nume, prenume, &team.players[i].points);
         team.players[i].firstName = (char *)calloc(strlen(nume) + 1, sizeof(char));
         team.players[i].secondName = (char *)calloc(strlen(prenume) + 1, sizeof(char));
         strcpy(team.players[i].firstName, nume);
@@ -36,27 +47,24 @@ Team Read_The_Team(FILE *d)
     team.teamPoints = (float)team.teamPoints / team.nr_players;
     return team;
 }
-// uata din curs si adauga la inceput date
-void AddAtBeginning(Node **head, Team team)
+
+
+//functia adauga la inceput echipele 
+void AddAtBeginning(Node **head,Team team)
 {
     Node *newNode = malloc(sizeof(Node));
     newNode->team = team;
     newNode->next = *head;
     *head = newNode;
 }
-// luata din curs si adauga la inceput date
-void AddAtEnd(Node **head, Team team)
-{
-    Node *aux = *head;
-    Node *newNode = (Node *)malloc(sizeof(Node));
+void AddAtEnd(Node** head, Team team) {
+    Node* aux = *head;
+    Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->team = team;
-
-    if (*head == NULL)
-    {
+    
+    if (*head == NULL) {
         AddAtBeginning(head, team);
-    }
-    else
-    {
+    } else {
         while (aux->next != NULL)
             aux = aux->next;
 
